0056e79270abd5bdc9cf289976d71c19d0f52510
Tommy Kärnström <tommy.karnstrom@weahead.se>
2019-10-03 10:54:40 +0200

    chore(repo): Removed comment

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const bootstrap = require('commitizen/dist/cli/git-cz').bootstrap;
 
 bootstrap({
   cliPath: path.join(__dirname, '../../node_modules/commitizen'),
-  // this is new
   config: {
     path: 'cz-conventional-changelog',
   },
